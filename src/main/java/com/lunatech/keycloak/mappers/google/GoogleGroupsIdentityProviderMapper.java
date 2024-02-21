ae673add57f8c24881bbf440c68a6f25ab9cdeb4
AmirAli Mashayekhi <amirali.mashayekhi@epita.fr>
2024-02-21 11:27:25 +0100

    Update GoogleGroupsIdentityProviderMapper.java Imported Google Provider

--- src/main/java/com/lunatech/keycloak/mappers/google/GoogleGroupsIdentityProviderMapper.java
+++ src/main/java/com/lunatech/keycloak/mappers/google/GoogleGroupsIdentityProviderMapper.java
@@ -2,7 +2,8 @@ package com.lunatech.keycloak.mappers.google;
 
 import com.github.slugify.Slugify;
 import org.keycloak.Config;
-import org.keycloak.broker.oidc.OIDCIdentityProviderFactory;
+// import org.keycloak.broker.oidc.OIDCIdentityProviderFactory;
+import org.keycloak.social.google.GoogleIdentityProviderFactory;
 import org.keycloak.broker.provider.AbstractIdentityProviderMapper;
 import org.keycloak.broker.provider.BrokeredIdentityContext;
 import org.keycloak.models.*;
@@ -65,7 +66,8 @@ public class GoogleGroupsIdentityProviderMapper extends AbstractIdentityProvider
 
     @Override
     public String[] getCompatibleProviders() {
-        return new String[]{ OIDCIdentityProviderFactory.PROVIDER_ID };
+        //return new String[]{ OIDCIdentityProviderFactory.PROVIDER_ID };
+        return new String[]{ GoogleIdentityProviderFactory.PROVIDER_ID };
     }
 
     @Override
